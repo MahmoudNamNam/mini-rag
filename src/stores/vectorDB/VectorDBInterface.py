a36a476fc815fb2acfe215e78592b1dd6e43e8fc
nmnm <mahmoud.namnam72@gmail.com>
2025-08-03 22:10:07 +0300

    add RetrievedDocument as output

--- src/stores/vectorDB/VectorDBInterface.py
+++ src/stores/vectorDB/VectorDBInterface.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import List
+from models.db_schemes import RetrievedDocument
 
 class VectorDBInterface(ABC):
 
@@ -54,6 +55,6 @@ class VectorDBInterface(ABC):
         pass
 
     @abstractmethod
-    def search_by_vector(self, collection_name: str, vector: list, limit: int) -> List:
+    def search_by_vector(self, collection_name: str, vector: list, limit: int) -> List[RetrievedDocument]:
         """Search by embedding vector."""
         pass
